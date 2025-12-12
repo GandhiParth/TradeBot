6011a07b5f7909f488c62bc4ac3063ea6e3a9672
GandhiParth <parthg1910@gmail.com>
2025-12-12 21:53:15 +0530

    added conf

---
+++ conf.py
@@ -0,0 +1,10 @@
+download_path = "./downloads"
+
+kite = {
+    "kite_cred_path": "/home/parthgandhi/TradeBot_archive/credentials/kite.ini",
+    "kite_conf_path": "./src/brokers/kite/conf.ini",
+    "hist_table_name": "historical",
+    "failed_hist_table_name": "historical_failed",
+}
+
+db_conn = "sqlite:///./downloads/data.db"
--- credentials.py
+++
@@ -1,3 +0,0 @@
-kite_path = "/home/parthgandhi/TradeBot_archive/credentials/kite.ini"
-
-download_path = "/home/parthgandhi/TradeBot/downloads"
