25745421c5d4935729d5ac5be98ab411c7eec2f1
GandhiParth <parthg1910@gmail.com>
2025-12-13 16:05:11 +0530

    save

--- src/scans/conf.py
+++ src/scans/conf.py
@@ -4,5 +4,5 @@ filters_dict = {
     1: 4.99,  # close today ≥ close 1 day ago * 1.05
     22: 10,  # close today ≥ close 22 days ago * 1.10
     67: 20,  # close today ≥ close 67 days ago * 1.22
-    # 125: 60,  # close today ≥ close 125 days ago * 1.60
+    125: 60,  # close today ≥ close 125 days ago * 1.60
 }
